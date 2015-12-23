141f0520c6c5051b9e86ba727d4ba4b91bf8aa55
Dhruv Pancholi <dhruv.pancholi@flipkart.com>
2015-12-23 17:04:10 +0530

    if-else

--- src/i_am_a_horse_in_the_land_of_booleans.clj
+++ src/i_am_a_horse_in_the_land_of_booleans.clj
@@ -9,10 +9,16 @@
 )
 
 (defn divides? [divisor n]
-  ":(")
+  (= (mod n divisor) 0))
 
 (defn fizzbuzz [n]
-  ":(")
+  (cond
+    (divides? 3 n)  "fizz"
+    (divides? 5 n)  "buzz"
+    (divides? 15 n) "gotcha!"
+    :else           ""
+    )
+)
 
 (defn teen? [age] 
   (if (and (> age 12) (< age 20))
@@ -23,7 +29,12 @@
   ":(")
 
 (defn generic-doublificate [x]
-  ":(")
+  (cond
+    (number? x) (* 2 x)
+    (empty? x) nil
+    (or (list? x) (vector? x)) (* 2 (count x))
+    :else true
+    ))
 
 (defn leap-year? [year]
   ":(")
